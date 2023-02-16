d0893389ecf221c93a392332519183e5c8c0a858
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2023-02-15 21:54:38 -0300

    OLA MUNDIO
    MUENDIO

---
+++ variaveis-funcoes.js
@@ -0,0 +1,14 @@
+// tipo number
+
+const Num1 = 3;
+const Num2 = 7;
+
+const Soma = Num1 + Num2;
+const Multiplicacao = Num1 * Num2;
+
+const Subtracao = Num1 - Num2;
+
+
+console.log (Soma);
+
+console.log (Multiplicacao);
